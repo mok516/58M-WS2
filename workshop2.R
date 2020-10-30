1e7f8db61796267dd368013dacdf42096dc18fdc
Molly K <mok516@york.ac.uk>
2020-10-30 11:51:15 +0000

    pipeline the plot save new data into raw data file

--- workshop2.R
+++ workshop2.R
@@ -31,3 +31,52 @@ hdi_summary <- hdi3 %>%
             sd = sd(Index),
             se = sd/sqrt(n))
 
+# filter the summary to get the 10 countries with the lowest mean HDI 
+hdi_summary_low <- hdi_summary %>% 
+  filter(rank(mean_index) < 11)
+hdi_summary_low
+
+# plot these low mean countries
+hdi_summary_low %>% 
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_index - se,
+                    ymax = mean_index + se)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
+
+
+# now build a pipeline that takes the hdi data frame (6340 rows) through to the plot with no intermediates 
+hdi_summary <- hdi3 %>%
+  group_by(Country) %>%
+  summarise(mean_index = mean(Index),
+            n = length (Index),
+            sd = sd(Index),
+            se = sd/sqrt(n)) %>% 
+  filter(rank(mean_index) < 11) %>% 
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_index - se,
+                    ymax = mean_index + se)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
+
+  
+  
+## Section 2 ##
+
+
