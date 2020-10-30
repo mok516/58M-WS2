0f43e73f86c0e1b23a385f5cc671a78156c5ce51
Molly K <mok516@york.ac.uk>
2020-10-30 11:07:13 +0000

    tidy data

--- workshop2.R
+++ workshop2.R
@@ -4,8 +4,17 @@ library(tidyverse)
 # read in data file 
 file <- "C:/Users/molsk/R work/Stage 3/58M-WS2/raw_data/Human-development-index.csv"
 
-human <- read_table2(file)
+human <- read_csv(file)
 # view data
 view(human)
 str(human)
 # not tidy at all 
+
+# tidy with pivot_longer 
+hdi <- human %>%
+  pivot_longer(names_to = "Years",
+               values_to = "HDI Index",
+               cols = -c(Country, `HDI Rank (2018)`))
+
+view(hdi)
+
